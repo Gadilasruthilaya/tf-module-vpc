093945b3e2c2f0adc2a72d1e6a3e5fcac875176e
sruthilaya <sruthilayagadila@gmail.com>
2024-01-03 22:09:25 +0530

    vpc

--- main.tf
+++ main.tf
@@ -1,4 +1,18 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
   enable_dns_support = true
+
+ tags = { merge({Name = "${var.env}-vpc"
+}, var.tags)
+
+ }
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.web_subnet_cidr_block
+
+  tags = { merge({Name = "${var.env}-subnet-web"}, var.tags)
+
+  }
 }
--- varibales.tf
+++ varibales.tf
@@ -1,1 +1,3 @@
 variable "cidr_block" {}
+variable "web_subnet_cidr_block" {}
+variable "env" {}
