a44c84b5f54988396b9ea9cc9a960286fce67ed4
sruthilaya <sruthilayagadila@gmail.com>
2024-01-04 00:38:09 +0530

    vpc

--- main.tf
+++ main.tf
@@ -20,3 +20,10 @@ module "subnets" {
   az = var.az
 
 }
+
+
+resource "aws_vpc_peering_connection" "foo" {
+  peer_vpc_id   = aws_vpc.main.id
+  vpc_id        = var.default_vpc_id
+  auto_accept = true
+}
--- variables.tf
+++ variables.tf
@@ -6,3 +6,9 @@ variable "tags" {}
 variable "az" {
   default = ["us-east-1a", "us-east-1b"]
 }
+
+variable "default_vpc_id" {}
+
+variable "subnet_id" {}
+
+variable "vpc_id" {}
